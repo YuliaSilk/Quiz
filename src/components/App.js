465a0c84844425fc40077f3c4840499334131975
YuliaSilk <129064716+YuliaSilk@users.noreply.github.com>
2023-10-11 23:46:37 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,38 @@
 import { Component } from "react";
 import { QuizList } from "./QuizList/QuizList";
 import initialQuizItems from './data.json';
+import { SearchBar } from "./SearchBar/SearchBar";
+import { QuizForm } from "./QuizForm/QuizForm";
 
 
 export class App extends Component {
   state = {
-    quizItems: initialQuizItems ,
+    quizItems: initialQuizItems,
+    topicFilter: '',
+    levelFilter: 'all',
   };
+changeTopicFilter = newTopic => {
+  this.setState({
+    topicFilter: newTopic,
+  })
+}
+
+changeLevelFilter = newLevel => {
+  this.setState({
+    levelFilter: newLevel,
+  })
+}
 
   render() {
-    const { quizItems } = this.state;
+    const { quizItems, topicFilter, levelFilter } = this.state;
     return <div>
+      <QuizForm/>
+      <SearchBar 
+      level={levelFilter} 
+      topic={topicFilter}
+      onChangeTopic={this.changeTopicFilter}
+      onChangeLevel={this.changeLevelFilter}
+      />
       <QuizList items={quizItems}/>
     </div>
   };
